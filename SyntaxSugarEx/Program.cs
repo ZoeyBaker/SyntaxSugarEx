87de6eebcad1067b141c1295c5e882ff061abb22
Zoey Baker <zoeybaker625@gmail.com>
2022-06-03 08:19:55 -0500

    initial commit

---
+++ SyntaxSugarEx/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SyntaxSugarEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Math is bad for you");
+
+
+        
+
+            {
+                // string response;
+
+                //  if (answer< 9)
+
+                //   {
+                //         response = answer + " is less than nine";
+                //     }
+                //      else
+                //      {
+
+                //         response = answer + "greater than or equal to nine";
+                //     }
+                var answer = 4;
+                var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+                Console.WriteLine(response);
+
+            }
+        }
+}    }   
